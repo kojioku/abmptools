ad2925ee32f2f58996800c282109930b53e532c4
Koji Okuwaki <koujioku81@gmail.com>
2019-05-25 00:21:28 +0900

    add sample dopc/cholesterol

--- udf_to_pdb.py
+++ udf_to_pdb.py
@@ -16,13 +16,50 @@ if __name__ == "__main__":
         print(" python udf_to_pdf.py filename")
         quit()
 
+    fff = udio.udfrm_io()
+
+    ## get tgtrec and move
+    tgtflag = False
+    moveflag = True
+    for ival in range(1, argc):
+        if argvs[ival] == "--rec":
+            tgtrec = argvs[ival + 1]
+            tgtflag = True
+        if argvs[ival] == "--nomove":
+            moveflag = False
+        if argvs[ival] == "--mol":
+            tgtmol = argvs[ival + 1]
+            fff.molflag = True
+
+    if tgtflag == False:
+        tgtrec = -1
+
+    if fff.molflag == False:
+        tgtmol = -1
+
+    recskip = False
+    molskip = False
     calc_args = []
-    for ifile in range(1, argc):
-        filename = argvs[ifile]
-        calc_args.append(filename)
+    for ival in range(1, argc):
+        filename = argvs[ival]
+        if recskip == True:
+            recskip = False
+            continue
+        if molskip == True:
+            molskip = False
+            continue
+        if argvs[ival] == "--rec":
+            recskip = True
+            continue
+        if argvs[ival] == "--mol":
+            molskip = True
+            continue
+        if argvs[ival] == "--nomove":
+            continue
+        calc_args.append([filename, int(tgtrec), int(tgtmol), moveflag])
         # main(filename)
 
-    fff = udio.udfrm_io()
+    print(calc_args)
 
     p = Pool()
     p.map(fff.run_convert, calc_args)
