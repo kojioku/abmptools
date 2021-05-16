7c560a3d66c78744f2eee56775831cccabc527ee
okuwaki <koujioku81@gmail.com>
2021-05-17 00:37:55 +0900

    update

---
+++ tips/createbatchrun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+stime=1
+etime=20
+interval=1
+script='runab22_fgk202105.sh'
+temp='1eo8-gmxtrr-wat4ang-xxx-renamed-MP2-6-31Gd-nbo.ajf'
+fhead=${temp%xxx*}
+ftail=${temp##*xxx}
+
+for i in `seq $stime $interval $etime`
+do
+    bash $script $fhead$i$ftail
+done
