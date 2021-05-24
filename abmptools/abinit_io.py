4d7dac99d713707d7b6d773e9168791d48a673e9
okuwaki <koujioku81@gmail.com>
2021-05-24 11:40:42 +0900

    update

--- abmptools/abinit_io.py
+++ abmptools/abinit_io.py
@@ -50,7 +50,7 @@ class abinit_io(mi.mol_io):
         self.pbcnv = 1.0
         self.piedaflag = True
         self.cpfflag = False
-        self.cpfver='23'
+        self.cpfver='10'
         self.cmmflag = False
         self.nofzc_flag = False
         self.bsseflag = False
@@ -222,10 +222,10 @@ ReadGeom='""" + str(self.readgeom) + "'\n"
 
         if self.cpfflag == True:
             ajf_body += "WriteGeom=" + str(self.writegeom) + "\n"
+            ajf_body += "CPFVER=" + str(self.cpfver) + "\n"
         ajf_body += """Gradient='NO'
 Vector='OFF'
 CPFBIN='NO'
-CPFVER=""" + str(self.cpfver) + """
 THOVL=1.0E-12
 E_THSWZ=1.0E-12
 G_THSWZ=1.0E-12
