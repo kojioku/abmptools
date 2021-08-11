1ba2e3c223700de2448cb8316e2d22335ae24ac3
okuwaki <koujioku81@gmail.com>
2021-08-11 21:42:20 +0900

    clean (v1.9.0)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if 'install' in argvs:
 
 setup(
     name='ABMPTools',
-    version='1.8.2',
+    version='1.9.0',
     description='setup tool for ABINIT-MP',
     long_description=readme,
     install_requires=['numpy', 'pandas'],
