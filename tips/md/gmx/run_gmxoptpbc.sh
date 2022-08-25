02197047e63f724d87a813800925cf1c1f043292
okuwaki <koujioku81@gmail.com>
2022-08-25 22:04:35 +0900

    update gmx tool and pdb sort

--- tips/md/gmx/run_gmxoptpbc.sh
+++ tips/md/gmx/run_gmxoptpbc.sh
@@ -6,11 +6,9 @@
 # cuttrajgmx-foropt.sh: file for generating cut-pdb
 
 ##################
-centerinfo=":1-381"
-maskinfo=":1-381"
-
-stripdist1=6.0
-stripdist2=4.0
+centerinfo=":1-383,596"
+maskinfo=":1-383,596"
+stripdist=4.0
 
 startframe=1001
 endframe=2001 #2001
@@ -18,6 +16,9 @@ interval=20
 
 stimeps=10000  # please specify the start time of production run
 intervalps=200
+
+optomp=8
+optmpi=2
 
 is_rmNaClion=true
 ##################
@@ -55,7 +56,7 @@ head=${headbuf##*/}
 #step0 create cut-pdb
 #bash cuttrjgmx-foropt.sh 4j0p-1H8_e10ps.prmtop 4j0p-1H8_md-samp_e10ps.xtc
 
-function step0_cut-pdb(){
+function step0_cuttrj(){
 curtime=$(date "+%Y%m%d-%H%M")
 if [ -d $dir ]; then
     echo "Backoff! old $dir is renamed to ${dir}.bak.${curtime}"
@@ -71,18 +72,18 @@ echo "autoimage anchor $centerinfo origin" >> cpptraj.in
 timeps=${stimeps}
 for i in `seq $startframe $interval $endframe`
 do
-    newtraj=${head}_${timeps}ps.pdb
+    newtraj=${head}_${timeps}ps.rst
     echo "trajout $newtraj onlyframes $i restart" >> cpptraj.in
     timeps=`echo "$timeps + $intervalps" | bc`
 done
 echo 'run' >> cpptraj.in
 cpptraj < cpptraj.in
 
-mv ${head}*.pdb $dir
+mv ${head}*.rst $dir
 cp $prmtop $dir
-echo "$dir/*.pdb was generated."
+echo "$dir/*.rst was generated."
 
-ofile="getcutpdb.${curtime}.log"
+ofile="getcutrst.${curtime}.log"
 
 echo `date` >> $ofile
 echo $prmtop >> $ofile
@@ -90,114 +91,90 @@ echo $traj >> $ofile
 echo $startframe, $endframe, $interval >> $ofile
 }
 
-#step1 strip prmtop
-function step1_strip-prmtop(){
-cd ${dir}
-tgtstr=${head}_${tgtps}ps
-echo "
-parm ${prmtop}
-parminfo *
-reference ${tgtstr}.pdb
-parmstrip (${maskinfo}>:${stripdist1})&!:NA+&!:NA&!:Na+&!:CL-&!:CL&!:Cl-
-parmwrite out ${tgtstr}-strip.prmtop
-" > cpptraj_strip.in
-cpptraj < cpptraj_strip.in
-}
 
-#step2 mask str
-function step2_mask-str(){
-tgtstr=${head}_${tgtps}ps
-echo "
-parm ${prmtop}
-trajin ${tgtstr}.pdb
-reference  ${tgtstr}.pdb
-mask (${maskinfo}<:${stripdist1})|:NA+|:NA|:Na+|:CL|:Cl- maskpdb ${tgtstr}-strip.pdb
-run
-" > cpptraj_mask.in
-cpptraj < cpptraj_mask.in
-}
-
-#step3 pdb2trj
-function step3_pdb2trj(){
-tgtstr=${head}_${tgtps}ps
-echo "
-parm ${tgtstr}-strip.prmtop
-trajin ${tgtstr}-strip.pdb.1
-trajout ${tgtstr}-strip.trj onlyframes 1 restart
-run
-quit
-" > cpptraj_pdb2trj.in
-cpptraj < cpptraj_pdb2trj.in
-}
 
 #step4 acpype
 function step4_acpype(){
+cd ${dir}
 tgtstr=${head}_${tgtps}ps
-acpype -p ${tgtstr}-strip.prmtop -x ${tgtstr}-strip.trj
+acpype -p ${prmtop} -x ${tgtstr}.rst -b ${tgtstr}
 }
 
 #step5 edit vaccum energy minimize setting
 function step5_edit-vaccum-emset(){
 tgtstr=${head}_${tgtps}ps
-cd ${tgtstr}-strip.amb2gmx
+cd ${tgtstr}.amb2gmx
 echo "
 integrator    = steep
+nsteps        = 10000
 emtol         = 1000
 emstep        = 0.01
-nsteps        = 50000
-nstxout       = 100
+nstcomm       = 100
+comm-mode     = Linear
+comm-grps     = System
 
-nstlist       = 1
-cutoff-scheme = Verlet
+nstlog        = 1
+nstxout       = 0
+nstvout       = 0
+nstfout       = 0
+nstlist       = 100
 ns_type       = grid
-coulombtype   = Cut-off
-rcoulomb      = 333.3
-vdwtype = Cut-off
-rvdw          = 333.3
 pbc           = xyz
-rlist         = 333.3
+rlist         = 1.0
+coulombtype   = PME
+rcoulomb      = 1.2
+rvdw          = 1.2
+vdwtype       = cut-off
+constraints   = none
+cutoff-scheme = Verlet
 " > em.mdp
 }
 
 #step6 add restraint to top
 function step6-9_emrun(){
 tgtstr=${head}_${tgtps}ps
-cat posre_${tgtstr}-strip.itp >>  ${tgtstr}-strip_GMX.top
+cat posre_${tgtstr}.itp >>  ${tgtstr}_GMX.top
 
 #step7 set big box
-gmx editconf -f ${tgtstr}-strip_GMX.gro -o ${tgtstr}-strip_GMXbox.gro -d 700 -bt cubic
+# gmx editconf -f ${tgtstr}_GMX.gro -o ${tgtstr}_GMXbox.gro -d 700 -bt cubic
 
 #step8 create tpr
-gmx grompp -f em.mdp -c ${tgtstr}-strip_GMXbox.gro -r ${tgtstr}-strip_GMXbox.gro -p ${tgtstr}-strip_GMX.top -o ${tgtstr}-strip_GMX.tpr
+gmx grompp -f em.mdp -c ${tgtstr}_GMX.gro -r ${tgtstr}_GMX.gro -p ${tgtstr}_GMX.top -o ${tgtstr}_GMX.tpr
 
 #step9 run
-gmx mdrun -ntomp 2 -v -deffnm ${tgtstr}-strip_GMX
+gmx mdrun -ntomp ${optomp} -ntmpi ${optmpi} -v -deffnm ${tgtstr}_GMX
+
+echo "0
+0" > conv.in
+gmx trjconv -s ${tgtstr}_GMX.tpr -f ${tgtstr}_GMX.trr -o ${tgtstr}_GMXjump.trr -pbc nojump -center < conv.in
+
 }
 
 #step10 getlastpdb
 function step10_getlastpdb(){
 tgtstr=${head}_${tgtps}ps
 echo "
-parm ../${tgtstr}-strip.prmtop
-trajin ${tgtstr}-strip_GMX.trr lastframe
+parm ../${prmtop}
+trajin ${tgtstr}_GMXjump.trr lastframe
+unwrap ${maskinfo}
 autoimage anchor ${centerinfo} origin
-trajout ${tgtstr}-strip_GMX.pdb
+trajout ${tgtstr}_GMXjump.pdb
 run
 quit
 " > cpptraj_genoptpdb.in
 cpptraj < cpptraj_genoptpdb.in
 
 echo "
-parm ../${tgtstr}-strip.prmtop
-trajin ${tgtstr}-strip_GMX.pdb
-reference ${tgtstr}-strip_GMX.pdb
-mask (${maskinfo}<:${stripdist2})|:NA+|:NA|:Na+|:CL|:Cl- maskpdb ${tgtstr}-optmasked-${stripdist2}ang.pdb
+parm ../${prmtop}
+trajin ${tgtstr}_GMXjump.pdb
+reference ${tgtstr}_GMXjump.pdb
+mask (${maskinfo}<:${stripdist})|:NA+|:NA|:Na+|:CL|:Cl- maskpdb ${tgtstr}-optmasked-${stripdist}ang.pdb
 run
 quit
 " > cpptraj_genoptpdb2.in
 cpptraj < cpptraj_genoptpdb2.in
 
-mv ${tgtstr}-optmasked-${stripdist2}ang.pdb.1 ${tgtstr}-optmasked-${stripdist2}ang.pdb
+mv ${tgtstr}-optmasked-${stripdist}ang.pdb.1 ${tgtstr}-optmasked-${stripdist}ang.pdb
 }
 
 function stepop1_rmnaclion(){
@@ -208,20 +185,18 @@ do
 done
 }
 
-step0_cut-pdb
+step0_cuttrj
 timeps=${stimeps}
 for i in `seq $startframe $interval $endframe`
 do
     tgtps=${timeps}
-    step1_strip-prmtop
-    step2_mask-str
-    step3_pdb2trj
     step4_acpype
     step5_edit-vaccum-emset
     step6-9_emrun
     step10_getlastpdb
     timeps=`echo "$timeps + $intervalps" | bc`
     cd ../../
+    # cd ../
 done
 
 #last copy optedpdb
