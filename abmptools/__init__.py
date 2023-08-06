a90e298658ddf0d013dfd41af2e702954432e900
okuwaki <koujioku81@gmail.com>
2023-08-07 03:07:57 +0900

    update

--- abmptools/__init__.py
+++ abmptools/__init__.py
@@ -3,9 +3,8 @@ from .abinit_io import abinit_io
 from .setfmo import setfmo
 from .mol_io import mol_io
 from .molcalc import molcalc
-from .pdb_io import pdb_io
 from .udfrm_io import udfrm_io
 from .udf_io import udf_io
 from .udfcreate import udfcreate
 from .anlfmo import anlfmo
-
+from .cpfmanager import CPFManager
